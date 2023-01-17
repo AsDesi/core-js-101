c310cb6060a37e9ff98d44ed62aa72bae64ce8bd
AsDesi <desi80@inbox.ru>
2023-01-17 12:53:22 +0200

    feat: add code numbers

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -239,7 +239,7 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+
 }
 
 /**
@@ -256,7 +256,7 @@ function encodeToRot13(/* str */) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-  return typeof value === 'string';
+  return typeof value === 'string' || value instanceof String;
 }
 
 
